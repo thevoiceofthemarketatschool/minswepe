56eda48e6027b70e55dc9b80f583d8839c7f7a75
kaleo mercado <kaleo.mercado@student.csd509j.net>
2025-06-12 14:10:20 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 function cursorValid () {
     return mycursor.x > layer_game.left && mycursor.x < layer_game.right + 1 && (mycursor.y > layer_game.top && mycursor.y < layer_game.bottom + 1)
 }
+browserEvents.onMouseMove(function (x, y) {
+    mycursor.setPosition(Math.round(x), Math.round(y))
+})
 browserEvents.MouseLeft.onEvent(browserEvents.MouseButtonEvent.Pressed, function (x, y) {
     if (layer_game) {
         if (cursorValid() && layer_cover.image.getPixel(mycursor.x - (layer_game.left + 1), mycursor.y - (layer_game.top + 1)) == 10) {
@@ -14,23 +17,10 @@ browserEvents.MouseLeft.onEvent(browserEvents.MouseButtonEvent.Pressed, function
         }
     }
 })
-browserEvents.MouseRight.onEvent(browserEvents.MouseButtonEvent.Pressed, function (x, y) {
-    if (cursorValid() && layer_game) {
-        if (layer_cover.image.getPixel(mycursor.x - (layer_game.left + 1), mycursor.y - (layer_game.top + 1)) == 10) {
-            layer_cover.image.setPixel(mycursor.x - (layer_game.left + 1), mycursor.y - (layer_game.top + 1), 13)
-            if (layer_mines.image.getPixel(mycursor.x - (layer_game.left + 1), mycursor.y - 0) == 12) {
-                minesleft += -1
-            }
-        } else if (layer_cover.image.getPixel(mycursor.x - (layer_game.left + 1), mycursor.y - (layer_game.top + 1)) == 13) {
-            layer_cover.image.setPixel(mycursor.x - (layer_game.left + 1), mycursor.y - (layer_game.top + 1), 10)
-            if (layer_mines.image.getPixel(mycursor.x - (layer_game.left + 1), mycursor.y - 0) == 12) {
-                minesleft += 1
-            }
-        }
-    }
-})
 function startgame (width: number, height: number, mines: number) {
     minesleft = mines
+    totalmines = mines
+    flagsplaced = 0
     sprites.destroy(mycursor)
     layer_game = sprites.create(image.create(width, height), SpriteKind.Player)
     layer_mines = sprites.create(image.create(width, height), SpriteKind.Player)
@@ -39,6 +29,7 @@ function startgame (width: number, height: number, mines: number) {
     layer_mines.x = Math.round(scene.cameraProperty(CameraProperty.X))
     layer_mines.left = Math.round(layer_mines.left)
     layer_game.x = layer_mines.x
+    layer_cover.x = layer_mines.x
     layer_cover.x = layer_mines.x
     layer_mines.top = 21
     layer_game.top = 21
@@ -63,6 +54,23 @@ function startgame (width: number, height: number, mines: number) {
     layer_game.image.replace(0, 9)
     layer_cover.image.fill(10)
 }
+browserEvents.MouseRight.onEvent(browserEvents.MouseButtonEvent.Pressed, function (x, y) {
+    if (cursorValid() && layer_game) {
+        if (layer_cover.image.getPixel(mycursor.x - (layer_game.left + 1), mycursor.y - (layer_game.top + 1)) == 10) {
+            layer_cover.image.setPixel(mycursor.x - (layer_game.left + 1), mycursor.y - (layer_game.top + 1), 13)
+            flagsplaced += 1
+            if (layer_mines.image.getPixel(mycursor.x - (layer_game.left + 1), mycursor.y - (layer_game.top + 1)) == 12) {
+                minesleft += -1
+            }
+        } else if (layer_cover.image.getPixel(mycursor.x - (layer_game.left + 1), mycursor.y - (layer_game.top + 1)) == 13) {
+            layer_cover.image.setPixel(mycursor.x - (layer_game.left + 1), mycursor.y - (layer_game.top + 1), 10)
+            flagsplaced += -1
+            if (layer_mines.image.getPixel(mycursor.x - (layer_game.left + 1), mycursor.y - 0) == 12) {
+                minesleft += 1
+            }
+        }
+    }
+})
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     if (browserEvents.MouseLeft.isPressed()) {
         timer.throttle("action", 500, function () {
@@ -79,13 +87,12 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
         sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
     }
 })
-browserEvents.onMouseMove(function (x, y) {
-    mycursor.setPosition(Math.round(x), Math.round(y))
-})
 let offsett = 0
 let ofset = 0
 let ycheck = 0
 let xcheck = 0
+let flagsplaced = 0
+let totalmines = 0
 let layer_mines: Sprite = null
 let layer_cover: Sprite = null
 let layer_game: Sprite = null
@@ -104,11 +111,16 @@ mySprite = sprites.create(assets.image`hard`, SpriteKind.Enemy)
 mySprite.y += 24
 mycursor = sprites.create(assets.image`cursor`, SpriteKind.Projectile)
 game.onUpdateInterval(100, function () {
-    if (0 == gamestate) {
-        aliveornot.setImage(assets.image`lostgame`)
-    } else if (1 == gamestate) {
-        aliveornot.setImage(assets.image`playinggame`)
-    } else {
-        aliveornot.setImage(assets.image`wongame`)
+    if (layer_game) {
+        if (0 == gamestate) {
+            aliveornot.setImage(assets.image`lostgame`)
+        } else if (1 == gamestate) {
+            aliveornot.setImage(assets.image`playinggame`)
+            if (totalmines == flagsplaced && 0 == minesleft) {
+                gamestate = 3
+            }
+        } else {
+            aliveornot.setImage(assets.image`wongame`)
+        }
     }
 })
